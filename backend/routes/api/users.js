015300bd101dfb8faf4665185d0ce26fe2ea68d4
Sehbaz <sehbazm@gmail.com>
2019-07-23 17:52:54 +0100

    User registeration

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const { check, validationResult } = require("express-validator/check");
+const { check, validationResult } = require("express-validator");
+const User = require("../../models/User");
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
 
 // @route  POST api/users
 // @desc   Register User
@@ -23,8 +26,38 @@ router.post(
       return res.status(400).json({ errors: errors.array() });
     } else {
     }
-    console.log(req.body);
-    res.send("User route");
+    const { name, email, password } = req.body;
+
+    try {
+      async function init() {
+        let user = await User.findOne({ email });
+        if (user) {
+          return res
+            .status(400)
+            .json({ errors: [{ msg: "User already exists" }] });
+        }
+        const avatar = gravatar.url(email, { s: "200", r: "pg", d: "mm" });
+        user = new User({
+          name,
+          email,
+          avatar,
+          password
+        });
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(password, salt);
+        user.save();
+
+        res.send("User registered");
+      }
+      init();
+    } catch (error) {
+      console.error(error);
+      res.status(500).send("server error");
+    }
   }
 );
 module.exports = router;
+// check user exists
+// get users gravitar
+// encrypt password
+// return jwt
